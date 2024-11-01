da711f939ad84720acdb3426ef234635a63bfa16
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-11-01 04:59:58 -0300

    starting back end

--- src/app/Page/LeftSide.tsx
+++ src/app/Page/LeftSide.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Header } from '../components/Header'
+import { ButtonSearch } from '../components/ButtonSearch'
 
 export const LeftSide = () => {
   return (
-    <div>
+    <div className='ml-60'>
       <Header/>
-        <div className='flex flex-col gap-6 mt-12'>
+        <div className='flex flex-col gap-8 mt-20 '>
             <span className='flex items-center text-gray-500'>
             <span className='h-[0.1rem]  bg-gray-400 w-10 left pr-12 mr-5'></span>
             <p className=''>What Breed Is Your Dog?</p>
@@ -14,7 +15,7 @@ export const LeftSide = () => {
             Discover Your Dog's Breed!
             </h1>
             <p className='text-gray-500 lg:max-w-[40rem]'>Explore a Diverse Collection of Beautiful Photos Featuring the Breed You Think Most Accurately Represents Your Dog’s Unique Look and Personality!</p>
-
+            <ButtonSearch/>
         </div>
     </div>
   )
--- src/app/components/ButtonSearch.tsx
+++ src/app/components/ButtonSearch.tsx
@@ -1,10 +1,18 @@
-<a
-  className="group relative inline-block text-sm font-medium text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+import React from 'react'
+
+export const ButtonSearch = () => {
+  return (
+    <div>
+        <a
+  className="group relative inline-block text-sm font-medium text-[#EC9D59] focus:outline-none focus:ring active:text-indigo-500"
   href="#"
 >
   <span
-    className="absolute inset-0 translate-x-0.5 translate-y-0.5 bg-indigo-600 transition-transform group-hover:translate-x-0 group-hover:translate-y-0"
+    className="absolute inset-0 translate-x-0.5 translate-y-0.5 bg-[#EC9D59] transition-transform group-hover:translate-x-0 group-hover:translate-y-0"
   ></span>
 
-  <span className="relative block border border-current bg-white px-8 py-3"> Download </span>
+  <span className="relative block border border-current bg-[#a0501b] px-10 py-3"> Search </span>
 </a>
+    </div>
+  )
+}
